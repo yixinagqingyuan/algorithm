6baada26bc04196b0343e905d7d2188afd8548c6
杨永生 <yangyongsheng@58.com>
2024-06-27 18:03:50 +0800

    add file

--- hasLoop.js
+++ hasLoop.js
@@ -6,6 +6,10 @@ const hasLoop = (head) => {
   // 所谓快慢指针，就是我弄一个快指针，弄一个慢指针 循环这个链表  如果最后能遇上，那就说明有环
   // 如果遇不上，那就残废了
   // 首先 我们当然要声明快慢指针了
+  // 首先要判空
+  if (head == null || head.next == null) {
+    return false
+  }
   let slow = head
   let fast = head
   // 然后 while 循环，条件是什么呢？ 他们都还有
@@ -13,7 +17,7 @@ const hasLoop = (head) => {
   while (slow && fast) {
     // 这个地方的赋值就是快慢指针的妙用
     slow = slow.next
-    fast = fast.next.next
+    fast = fast.next ? fast.next.next : fast.next
     // 相等就代表遇上了，
     if (slow === fast) {
       return true

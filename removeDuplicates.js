818f1bdfb511b7dc64e873bc90f87a912051d13a
杨永生 <yangyongsheng@58.com>
2024-07-16 20:55:43 +0800

    feat: 添加tag

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -45,4 +45,40 @@ const removeDuplicates1 = (str) => {
   }
   console.log(stack)
 }
-removeDuplicates1('abbaca')
+//removeDuplicates1('abbaca')
+
+// 给你一个字符串 s，「k 倍重复项删除操作」将会从 s 中选择 k 个相邻且相等的字母，并删除它们，使被删去的字符串的左侧和右侧连在一起。
+//你需要对 s 重复进行无限次这样的删除操作，直到无法继续为止。
+//在执行完所有删除操作后，返回最终得到的字符串。
+
+//输入：s = "deeedbbcccbdaa", k = 3
+//输出："aa"
+//解释：
+//先删除 "eee" 和 "ccc"，得到 "ddbbbdaa"
+//再删除 "bbb"，得到 "dddaa"
+//最后删除 "ddd"，得到 "aa"
+// 这一类型的题有很多衍生的题目 带一个 k 就是很重要的并且常考的题目之一
+const removeDuplicates2 = (str, k) => {
+  // 首先得有个 stack 存结果
+  const stack = []
+  // 接下来指定要遍历
+  for (let i = 0; i < str.length; i++) {
+    const s = str[i]
+    // 这时候prve 可能多个值的情况并且相加的时候都是一样的
+    // 每次遍历，就会取前一个，这样就能满足当这个删除之后，后头的和前头的又续上了
+    const prve = stack.pop()
+    // 当 prve没有，或者，prve不等于当前的值,所以我们取一个标本，0 的位置
+    if (!prve || prve[0] == s) {
+      // 现在我取出来了，那么现在我要在存进去，因为都不一样了
+      stack.push(prve)
+      stack.push(s)
+    } else if (prve.length < k - 1) {
+      // 相当于给当前这个地方累加
+      stack.push(prve + s)
+    }
+    // 如果不满足，那么我已经取出来了，就不要在放进去了,这时候就能筛选出来有问题超过3个的情况
+  }
+  console.log(stack.join(''))
+}
+
+removeDuplicates2('deeedbbcccbdaa', 3)
--- spiralOrder.js
+++ spiralOrder.js
@@ -30,10 +30,9 @@ const spiralOrder = (arr) => {
   let res = []
   // 接下来就要开始疯狂遍历了，由于他是一层套一层，所以，我们要有个while 判断
   // 也就是 他还能遍历的时候，指导他遍历不动了，再说
-  // 这里之所以为或者，就是因为可能左右还能遍历，但上下遍历不了了
+  // 这里之所以为并且，就是因为可能左右还能遍历，但上下遍历不了了
   // 或者上线还能遍历，左右遍历不了了
-  console.log(left, right, top, bottom)
-  while (left < right || top < bottom) {
+  while (left < right && top < bottom) {
     // 然后就是上下左右开始遍历
     // 首先是上左到右
     // 还有个问题，之所以是小于 是由于当前的这个一个的结束就是下一个的开始 最后的结束就是第一个的开始
@@ -54,16 +53,28 @@ const spiralOrder = (arr) => {
       res.push(arr[i][left])
     }
     // 一遍便利完了，就要给他缩小范围
-    if (right > left) {
-      left++
-      right--
+
+    left++
+    right--
+    top++
+    bottom--
+  }
+  // 接下来我们解决上头并且带来的坑，就是当有个地方等于 0 之后， 我们其实就相当于这种四周遍历就迎来了终结
+  // 我们只需要顺着当前的为 0 的这个方向，解决问题即可
+  console.log(left, right, top, bottom)
+  if (left === right) {
+    for (let i = top; i <= bottom; i++) {
+      res.push(arr[i][left])
     }
-    if (bottom > top) {
-      top++
-      bottom--
+  } else if (top === bottom) {
+    for (let i = left; i <= right; i++) {
+      res.push(arr[top][i])
     }
   }
-
   console.log(res)
 }
-spiralOrder([[1, 2, 3, 4]])
+spiralOrder([
+  [1, 2, 3, 4],
+  [1, 2, 3, 4],
+  [1, 2, 3, 4],
+])

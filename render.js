3b81b0250d080ec150602b80598f46f308d56782
杨永生 <yangyongsheng@58.com>
2024-08-27 21:14:33 +0800

    feat: add file

---
+++ render.js
@@ -0,0 +1,23 @@
+/** @format */
+
+var template = '{{nnnn}}很厉害，才{{age}}岁'
+var context = { nnnn: 'bottle', age: '15' }
+//输入：template context
+//输出：bottle很厉害，才15岁
+//这一题其实就是字符串模板匹配
+// 主要就是利用正则解决问题
+// 此时首先我们应该有一个函数吧
+const render = () => {
+  // 然后弄一个正则表达式
+  // 这个正则表达式我懂了，当.*? 单独出现的时候，是单独匹配，但当他跟别的字符一块出现的时候，那么就会全量匹配
+  // 将中间的能匹配到的内容全部匹配上去要跟后头的对齐从而也能匹配到后都的内容属于贪婪匹配的并且贪婪一次
+  // 如果不加？ 就贪婪到最后一次
+  const reg = /\{\{(.*?)\}\}/
+  return template.replace(reg, (item, $1) => {
+    console.log(item, $1)
+    return context[$1]
+  })
+  // console.log(template)
+}
+// console.log(render())
+render()
